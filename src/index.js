dbc3d4bc4274c6566169f18edd7772908b4a5207
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-14 21:49:35 +0200

    using SignInForm

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SignInForm from './components/SignInForm'
 
-
-
-const App = () => {
-    return (<h1>Sign in </h1>)
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SignInForm />, document.getElementById('root'));
