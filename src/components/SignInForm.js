0fca25b9ffdaeef627a9800be951ce711329d79f
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-14 22:35:35 +0200

    signing in works

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import loginService from '../services/loginService'
+
 
 class SignInForm extends React.Component {
 
@@ -7,14 +9,55 @@ class SignInForm extends React.Component {
         this.state = {
             username: "",
             password: "",
-            token: ""
+            user: ""
         }
+    }
+
+    handleFormChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state[e.target.name])
+    }
+
+    logIn = async (e) => {
+        e.preventDefault();
+        console.log("login funktiossa!")
+        const credentials = {
+            username: this.state.username,
+             password: this.state.password
+            }
+        const response = await loginService.login(credentials)
+        console.log("response.data",response.data)
+        this.setState({
+            username: "",
+            password: "",
+            user: response.data.token
+        })
+        console.log("TOKEN: ", this.state.user)
     }
 
     render() {
         return (
         <div>
-            <h2>Sign in</h2>          
+            <h2>Welcome to blog-app!</h2>
+            <form onSubmit={this.logIn}>
+                Username:
+                <input 
+                    name = "username"
+                    value = {this.state.username}
+                    onChange = {this.handleFormChange}
+                 />  
+                 <br></br>
+                Password:
+                <input 
+                    name = "password"
+                    value = {this.state.password}
+                    onChange = {this.handleFormChange}
+                 />   
+                 <br></br>
+                 <button type="submit">Log in </button>
+            </form>          
         </div>)
     }
 }
