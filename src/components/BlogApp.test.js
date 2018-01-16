1546091fc6a3e429e2a6cfa4fb6feac7c58a9a14
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-01-16 15:56:46 +0200

    improved test

--- src/components/BlogApp.test.js
+++ src/components/BlogApp.test.js
@@ -3,6 +3,7 @@ import { mount} from 'enzyme'
 import BlogApp from './BlogApp'
 import SignInForm from './signin/SignInForm'
 import setUpTests from '../setup/setUpTests'
+import Blog from './blog/Blog'
 
 describe('<BlogApp />', () => {
     let app
@@ -16,8 +17,12 @@ describe('<BlogApp />', () => {
       it('only login form is rendered', () => {
         const blogApp = mount(<BlogApp />)
         const form = blogApp.find(SignInForm)
+        const blogs = blogApp.find(Blog)
         expect(form.length).toBe(1)
+        expect(blogs.length).toBe(0)
       })
+
+      
     })
   
   })
