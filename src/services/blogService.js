089dbefc25ac21e1ff215f0e9ad515758aa38bb6
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-02-18 13:00:39 +0200

    changed baseUrl

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import loginService from './loginService'
-const baseUrl = "http://localhost:3001/api/blogs"
+const baseUrl = "/api/blogs"
 
 const getAll = () => {
     return axios.get(baseUrl)
