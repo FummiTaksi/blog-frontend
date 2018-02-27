e8be9c0840e94addadab0004e2f9f881ea824511
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2018-02-27 18:39:15 +0200

    implemented user listing

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,13 +1,51 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { userInitialization } from '../../reducers/userReducer'
 
 class UserList extends React.Component {
 
+    componentDidMount() {
+        this.props.userInitialization()
+    }
+
+    renderUsers = () => {
+        return this.props.users.map(user => {
+            return (
+                <div key = {user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.blogs.length}</td>
+                </div>
+            )
+        })
+    }
+
     render() {
         return (
-            <h1>Listing all users:</h1>
-        )
+            <div>
+              <h1>Listing all users:</h1>
+              <table>
+                  <tr>
+                      <th>Name</th>
+                      <th>Blogs added</th>
+                  </tr>
+                  {this.renderUsers()}
 
+
+              </table>
+            </div>
+        )
     }
 }
 
-export default UserList
+const mapStateToProps = (state) => {
+    return {
+        users: state.users
+    }
+}
+
+const ConnectedUserList = connect(
+    mapStateToProps,
+    {userInitialization}
+)(UserList)
+
+export default ConnectedUserList
